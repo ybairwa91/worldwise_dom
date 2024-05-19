8925aa663e302f5ba6428e7c7181cf513373365c
Yogeshbairwa <ybairwa786@gmail.com>
2024-05-19 16:55:12 +0530

    updats

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import "./index.css";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
