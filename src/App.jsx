c4fc7c37bb8d444607f77cb69e41bacee19f3c76
Yogeshbairwa <ybairwa786@gmail.com>
2024-05-19 16:31:15 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
-import PageNav from "./components/PageNav";
 
 function App() {
   return (
@@ -19,5 +18,3 @@ function App() {
 }
 
 export default App;
-
-// <h1>Hello Router!</h1>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,26 +1,4 @@
-// import styles from "./Homepage.module.css";
-
-import { Link } from "react-router-dom";
 import PageNav from "./../components/PageNav";
-
-// export default function Homepage() {
-//   return (
-//     <main className={styles.homepage}>
-//       <section>
-//         <h1>
-//           You travel the world.
-//           <br />
-//           WorldWise keeps track of your adventures.
-//         </h1>
-//         <h2>
-//           A world map that tracks your footsteps into every city you can think
-//           of. Never forget your wonderful experiences, and show your friends how
-//           you have wandered the world.
-//         </h2>
-//       </section>
-//     </main>
-//   );
-// }
 
 function Homepage() {
   return (
@@ -32,4 +10,3 @@ function Homepage() {
 }
 
 export default Homepage;
-// <a href="/pricing">Pricing</a>
