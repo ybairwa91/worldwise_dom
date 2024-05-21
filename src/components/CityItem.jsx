fdcec5231fd628d08997eb1957f5eaf602572c8c
Yogeshbairwa <ybairwa786@gmail.com>
2024-05-21 14:30:48 +0530

    updates

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,5 @@
+function CityItem() {
+  return <li>City</li>;
+}
+
+export default CityItem;
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,12 +1,18 @@
+import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
-  return <ul className={styles.cityList}>
-  {cities.map(city=><CityItem/>)}
-  </ul>;
+  console.log(cities);
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
