6733514e5ffc12ca7708d2dd6a417b245beb9449
Yogeshbairwa <ybairwa786@gmail.com>
2024-05-22 10:49:44 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import "./index.css";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -52,6 +53,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
